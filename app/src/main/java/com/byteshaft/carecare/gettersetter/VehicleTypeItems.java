45c0bb193cf895c09ed4d82824067de26c73ca8b
husnain hussi <husnain446@gmail.com>
2018-05-28 14:00:35 +0500

    add fragments details

---
+++ app/src/main/java/com/byteshaft/carecare/gettersetter/VehicleTypeItems.java
@@ -0,0 +1,23 @@
+package com.byteshaft.carecare.gettersetter;
+
+public class VehicleTypeItems {
+
+    private String vehicleTypeName;
+    private int vehicleTypeId;
+
+    public String getVehicleTypeName() {
+        return vehicleTypeName;
+    }
+
+    public void setVehicleTypeName(String vehicleTypeName) {
+        this.vehicleTypeName = vehicleTypeName;
+    }
+
+    public int getVehicleTypeId() {
+        return vehicleTypeId;
+    }
+
+    public void setVehicleTypeId(int vehicleTypeId) {
+        this.vehicleTypeId = vehicleTypeId;
+    }
+}
--- app/src/main/java/com/byteshaft/carecare/useraccounts/UserSignUp.java
+++ app/src/main/java/com/byteshaft/carecare/useraccounts/UserSignUp.java
@@ -12,8 +12,11 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.byteshaft.carecare.Adapters.VehicleMakes;
+import com.byteshaft.carecare.Adapters.VehicleType;
 import com.byteshaft.carecare.R;
-import com.byteshaft.carecare.gettersetter.CarModelItems;
+import com.byteshaft.carecare.gettersetter.VehicleMakeItems;
+import com.byteshaft.carecare.gettersetter.VehicleTypeItems;
 import com.byteshaft.carecare.utils.AppGlobals;
 import com.byteshaft.carecare.utils.Helpers;
 import com.byteshaft.requests.HttpRequest;
@@ -23,6 +26,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
 
 public class UserSignUp extends Fragment implements HttpRequest.OnReadyStateChangeListener,
         HttpRequest.OnErrorListener {
@@ -30,16 +34,21 @@ public class UserSignUp extends Fragment implements HttpRequest.OnReadyStateChan
     private View mBaseView;
 
     private Button mSignUpButtonButton;
-    private EditText mFirstNameEditText;
-    private EditText mLastNameEditText;
+    private EditText mFullNameEditText;
+    private EditText mUserNameEditText;
     private EditText mContactNumberEditText;
     private EditText mEmailAddressEditText;
     private EditText mPasswordEditText;
     private EditText mVerifyPasswordEditText;
     private EditText mAddressEditText;
     private TextView mPickForCurrentLocation;
-    private Spinner mCaModelSpinner;
-    private Spinner mCaTypeSpinner;
+    private Spinner mVehicleMakeSpinner;
+    private Spinner mVehicleTypeSpinner;
+
+    private VehicleMakes vehicleMakesAdapter;
+    private ArrayList<VehicleMakeItems> vehicleMakeArrayList;
+    private VehicleType vehicleTypeAdapter;
+    private ArrayList<VehicleTypeItems> vehicleTypeArrayList;
 
     private HttpRequest request;
 
@@ -49,22 +58,25 @@ public class UserSignUp extends Fragment implements HttpRequest.OnReadyStateChan
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBaseView = inflater.inflate(R.layout.fragment_user_sign_up, container, false);
 
-        mFirstNameEditText = mBaseView.findViewById(R.id.first_name_edit_text);
-        mLastNameEditText = mBaseView.findViewById(R.id.last_name_edit_text);
+        mFullNameEditText = mBaseView.findViewById(R.id.full_name_edit_text);
+        mUserNameEditText = mBaseView.findViewById(R.id.user_name_edit_text);
         mEmailAddressEditText = mBaseView.findViewById(R.id.email_edit_text);
         mContactNumberEditText = mBaseView.findViewById(R.id.contact_number_edit_text);
+
         mPasswordEditText = mBaseView.findViewById(R.id.password_edit_text);
         mVerifyPasswordEditText = mBaseView.findViewById(R.id.confirm_edit_text);
         mAddressEditText = mBaseView.findViewById(R.id.address_edit_text);
         mPickForCurrentLocation = mBaseView.findViewById(R.id.pick_for_current_location);
-        mCaModelSpinner = mBaseView.findViewById(R.id.car_model_spinner);
-        mCaTypeSpinner = mBaseView.findViewById(R.id.car_type_spinner);
 
+        mVehicleMakeSpinner = mBaseView.findViewById(R.id.vehicle_make_spinner);
+        mVehicleTypeSpinner = mBaseView.findViewById(R.id.vehicle_type_spinner);
+        vehicleMakeArrayList = new ArrayList<>();
+        vehicleTypeArrayList = new ArrayList<>();
         return mBaseView;
     }
 
-    private void getCarModels() {
-        HttpRequest getStateRequest = new HttpRequest(getActivity().getApplicationContext());
+    private void getVehicleMake() {
+        HttpRequest getStateRequest = new HttpRequest(getActivity());
         getStateRequest.setOnReadyStateChangeListener(new HttpRequest.OnReadyStateChangeListener() {
             @Override
             public void onReadyStateChange(HttpRequest request, int readyState) {
@@ -78,9 +90,14 @@ public class UserSignUp extends Fragment implements HttpRequest.OnReadyStateChan
                                     for (int i = 0; i < jsonArray.length(); i++) {
                                         System.out.println("Test " + jsonArray.getJSONObject(i));
                                         JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                        CarModelItems carModelItems = new CarModelItems();
-
+                                        VehicleMakeItems vehicleMakeItems = new VehicleMakeItems();
+                                        vehicleMakeItems.setVehicleMakeName(jsonObject.getString("name"));
+                                        vehicleMakeItems.setVehicleMakeId(jsonObject.getInt("id"));
+                                        vehicleMakeArrayList.add(vehicleMakeItems);
                                     }
+                                    vehicleMakesAdapter = new VehicleMakes(getActivity(), vehicleMakeArrayList);
+                                    mVehicleMakeSpinner.setAdapter(vehicleMakesAdapter);
+                                    mVehicleMakeSpinner.setSelection(0);
                                 } catch (JSONException e) {
                                     e.printStackTrace();
                                 }
@@ -88,36 +105,72 @@ public class UserSignUp extends Fragment implements HttpRequest.OnReadyStateChan
                 }
             }
         });
-        getStateRequest.open("GET", String.format("%sstates", AppGlobals.BASE_URL));
+        getStateRequest.open("GET", String.format("%svehicles/make", AppGlobals.BASE_URL));
         getStateRequest.send();
     }
 
-    private void registerUser(String full_name, String email, String phoneNumber,
-                              String address, String landline, String location, String sector,
-                              String username, String password) {
+    private void getVehicleType() {
+        HttpRequest getStateRequest = new HttpRequest(getActivity());
+        getStateRequest.setOnReadyStateChangeListener(new HttpRequest.OnReadyStateChangeListener() {
+            @Override
+            public void onReadyStateChange(HttpRequest request, int readyState) {
+                switch (readyState) {
+                    case HttpRequest.STATE_DONE:
+                        switch (request.getStatus()) {
+                            case HttpURLConnection.HTTP_OK:
+                                try {
+                                    JSONObject object = new JSONObject(request.getResponseText());
+                                    JSONArray jsonArray = object.getJSONArray("results");
+                                    for (int i = 0; i < jsonArray.length(); i++) {
+                                        System.out.println("Test " + jsonArray.getJSONObject(i));
+                                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                        VehicleTypeItems vehicleItems = new VehicleTypeItems();
+                                        vehicleItems.setVehicleTypeId(jsonObject.getInt("id"));
+                                        vehicleItems.setVehicleTypeName(jsonObject.getString("name"));
+                                        vehicleTypeArrayList.add(vehicleItems);
+                                    }
+                                    vehicleTypeAdapter = new VehicleType(getActivity(), vehicleTypeArrayList);
+                                    mVehicleTypeSpinner.setAdapter(vehicleTypeAdapter);
+                                    mVehicleTypeSpinner.setSelection(0);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                        }
+                }
+            }
+        });
+        getStateRequest.open("GET", String.format("%svehicles/type", AppGlobals.BASE_URL));
+        getStateRequest.send();
+    }
+
+    private void registerUser(String username, String name, String email, String contactNumber,
+                              String address, String addressCoordinates, String vehiclMake, String vehicleModel,
+                              String vehicleType, String vehicleYear, String password) {
         request = new HttpRequest(getActivity());
         request.setOnReadyStateChangeListener(this);
         request.setOnErrorListener(this);
-        request.open("POST", String.format("%sregister", AppGlobals.BASE_URL));
-        request.send(getRegisterData(full_name, email, phoneNumber, address, landline, location,
-                sector, username, password));
+        request.open("POST", String.format("%sregister-customer", AppGlobals.BASE_URL));
+        request.send(getRegisterData(username, name, email, contactNumber, address, addressCoordinates,
+                vehiclMake, vehicleModel, vehicleType, vehicleYear, password));
         Helpers.showProgressDialog(getActivity(), "Registering User ");
     }
 
 
-    private String getRegisterData(String full_name, String email, String phoneNumber,
-                                   String address, String landline, String location, String sector,
-                                   String username, String password) {
+    private String getRegisterData(String username, String name, String email, String contactNumber,
+                                   String address, String addressCoordinates, String vehiclMake, String vehicleModel,
+                                   String vehicleType, String vehicleYear, String password) {
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("full_name", full_name);
-            jsonObject.put("email", email);
-            jsonObject.put("mobile_number", phoneNumber);
-            jsonObject.put("address", address);
-            jsonObject.put("landline", landline);
-            jsonObject.put("location", location);
-            jsonObject.put("sector", sector);
             jsonObject.put("username", username);
+            jsonObject.put("name", name);
+            jsonObject.put("email", email);
+            jsonObject.put("contact_number", contactNumber);
+            jsonObject.put("address", address);
+            jsonObject.put("address_coordinates", addressCoordinates);
+            jsonObject.put("vehicle_make", vehiclMake);
+            jsonObject.put("vehicle_model", vehicleModel);
+            jsonObject.put("vehicle_type", vehicleType);
+            jsonObject.put("vehicle_year", vehicleYear);
             jsonObject.put("password", password);
         } catch (JSONException e) {
             e.printStackTrace();
