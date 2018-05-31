4e41d0136a448b70ec8dfa56bd4dcdccb94b88fe
husnain hussi <husnain446@gmail.com>
2018-05-31 17:50:25 +0500

    add services fragments

--- app/src/main/java/com/byteshaft/carecare/userFragments/CarWashFragment.java
+++ app/src/main/java/com/byteshaft/carecare/userFragments/CarWashFragment.java
@@ -7,19 +7,100 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
+import com.byteshaft.carecare.Adapters.AutoMechanicCarWashAdapter;
 import com.byteshaft.carecare.R;
+import com.byteshaft.carecare.gettersetter.AutoMechanicCarWashItems;
+import com.byteshaft.carecare.utils.AppGlobals;
+import com.byteshaft.carecare.utils.Helpers;
+import com.byteshaft.requests.HttpRequest;
 
-public class CarWashFragment extends Fragment {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+
+public class CarWashFragment extends Fragment implements HttpRequest.OnReadyStateChangeListener,
+        HttpRequest.OnErrorListener, View.OnClickListener {
 
     private View mBaseView;
     private ListView mCarWashListView;
+    private Button mNextButton;
+    private HttpRequest request;
+
+    private AutoMechanicCarWashAdapter adapter;
+    private ArrayList<AutoMechanicCarWashItems> arrayList;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBaseView = inflater.inflate(R.layout.fragment_car_wash, container, false);
+        mCarWashListView = mBaseView.findViewById(R.id.car_wash_list_view);
+        mNextButton = mBaseView.findViewById(R.id.button_next);
+        mNextButton.setOnClickListener(this);
+        arrayList = new ArrayList<>();
+        adapter = new AutoMechanicCarWashAdapter(getActivity(), arrayList);
+        mCarWashListView.setAdapter(adapter);
+        getCarWashServicesList();
         return mBaseView;
+    }
+
+    @Override
+    public void onError(HttpRequest request, int readyState, short error, Exception exception) {
+        Helpers.dismissProgressDialog();
+        switch (readyState) {
+            case HttpRequest.ERROR_CONNECTION_TIMED_OUT:
+                Helpers.showSnackBar(getView(), getString(R.string.connection_time_out));
+                break;
+            case HttpRequest.ERROR_NETWORK_UNREACHABLE:
+                Helpers.showSnackBar(getView(), exception.getLocalizedMessage());
+                break;
+        }
+
+    }
+
+    @Override
+    public void onReadyStateChange(HttpRequest request, int readyState) {
+        switch (readyState) {
+            case HttpRequest.STATE_DONE:
+                Helpers.dismissProgressDialog();
+                switch (request.getStatus()) {
+                    case HttpURLConnection.HTTP_OK:
+                        try {
+                            JSONArray jsonArray = new JSONArray(request.getResponseText());
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                System.out.println("Test " + jsonArray.getJSONObject(i));
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                AutoMechanicCarWashItems items = new AutoMechanicCarWashItems();
+                                items.setServiceId(jsonObject.getInt("id"));
+                                items.setServiceName(jsonObject.getString("name"));
+                                items.setServicePrice(jsonObject.getInt("price"));
+                                arrayList.add(items);
+                                adapter.notifyDataSetChanged();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                }
+        }
+
+    }
+
+    private void getCarWashServicesList() {
+        request = new HttpRequest(getActivity());
+        request.setOnReadyStateChangeListener(this);
+        request.setOnErrorListener(this);
+        request.open("GET", String.format("%svegetables/", AppGlobals.BASE_URL));
+        request.send();
+        Helpers.showProgressDialog(getActivity(), "Fetching Services...");
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
